daf83137e0bc57c60ddd8cb1cfcee206c71b6629
John Dorman <John Dorman>
2017-01-30 18:48:23 +0000

    Prototype of the initial game idea.

---
+++ GroupProjectPrototype/Assets/PlatformScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //GetComponent<Rigidbody>().AddForce(0, -300, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(0, -1.0f);
+
+        if(transform.position.y < -10.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ GroupProjectPrototype/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour
+{
+    //Variables
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        // is the controller on the ground?
+        
+        if (controller.isGrounded)
+        {
+            //Feed moveDirection with input.
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            moveDirection = transform.TransformDirection(moveDirection);
+            //Multiply it by speed.
+            moveDirection *= speed;
+            //Jumping
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.x = Input.GetAxis("Horizontal") * speed;
+        //Applying gravity to the controller
+        moveDirection.y -= gravity * Time.deltaTime;
+        //Making the character move
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        if (transform.position.z != 0)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.z = 0;
+            transform.position = newPosition;
+           // moveDirection.x = 0;
+           // moveDirection.y = 0;
+        }
+
+        
+    }
+}
